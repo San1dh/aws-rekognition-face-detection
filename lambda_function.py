d64ea0e7a207c78304bb8ea53cfbc0136d48aa4d
SIV <fathersson1302@gmail.com>
2023-11-18 16:28:51 +0530

    Update lambda with a Hardcoded Image & Rekognition to test it.

--- lambda_function.py
+++ lambda_function.py
@@ -1,9 +1,25 @@
 import json
+import boto3
+
+client = boto3.client('rekognition')
 
 def lambda_handler(event, context):
     # TODO implement
 
-    return {
-        'statusCode': 200,
-        'body': json.dumps('Hello from Lambda!')
-    }
+    bucket_name = "my-own-rekognition-bucket"
+    file_name = "img5.jpg"
+
+     response = client.detect_faces(
+            Image={
+                'S3Object':{
+                    'Bucket':bucket_name,
+                    'Name':file_name
+                }
+            },
+            Attributes = ['ALL']
+        )
+
+    # Process result
+    for face in response['FaceDetails']:
+        print(json.dumps(face, indent = 2))
+    
